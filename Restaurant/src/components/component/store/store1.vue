0363f967cdfbf368a8f24b18b2643f839e1a72b5
chenghao <chenghao@corp-ci.com>
2017-05-22 11:05:47 +0800

    完成门店信息

--- Restaurant/src/components/component/store/store1.vue
+++ Restaurant/src/components/component/store/store1.vue
@@ -1,58 +1,77 @@
 <template>
-  <el-form ref="form" :model="form" label-width="80px">
+  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px" class="demo-ruleForm">
     <el-form-item label="门店绑定手机号：">
-      <el-input v-model="form.name"></el-input>
+      18651908626
     </el-form-item>
-    <el-form-item label="门店名称">
-      <el-input v-model="form.name"></el-input>
+    <el-form-item label="门店名称" prop="storename">
+      <el-input v-model="ruleForm.storename"></el-input>
     </el-form-item>
-    <el-form-item label="活动区域">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
+    <el-form-item label="外卖电话" prop="phone">
+      <el-input v-model="ruleForm.name"></el-input>
+    </el-form-item>
+    <el-form-item label="联系人名称" prop="name">
+      <el-input v-model="ruleForm.name"></el-input>
+    </el-form-item>
+    <el-form-item label="门店分类" prop="storetype">
+      <el-select v-model="ruleForm.storetype" placeholder="请选择活动区域">
         <el-option label="区域一" value="shanghai"></el-option>
         <el-option label="区域二" value="beijing"></el-option>
       </el-select>
     </el-form-item>
-    <el-form-item label="活动时间">
-      <el-col :span="11">
-        <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-      </el-col>
-      <el-col class="line" :span="2">-</el-col>
-      <el-col :span="11">
-        <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-      </el-col>
-    </el-form-item>
-    <el-form-item label="即时配送">
-      <el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
-    </el-form-item>
-    <el-form-item label="活动性质">
-      <el-checkbox-group v-model="form.type">
-        <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-        <el-checkbox label="地推活动" name="type"></el-checkbox>
-        <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-        <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="特殊资源">
-      <el-radio-group v-model="form.resource">
-        <el-radio label="线上品牌商赞助"></el-radio>
-        <el-radio label="线下场地免费"></el-radio>
+    <el-form-item label="营业时间" prop="time">
+      <el-radio-group v-model="ruleForm.time">
+        <el-radio label="24小时"></el-radio>
+        <el-radio label="自定义"></el-radio>
       </el-radio-group>
     </el-form-item>
-    <el-form-item label="活动形式">
-      <el-input type="textarea" v-model="form.desc"></el-input>
+    <el-form-item label="城市" prop="city">
+      <el-input v-model="ruleForm.city"></el-input>
     </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">立即创建</el-button>
-      <el-button>取消</el-button>
+    <el-form-item label="详细地址" prop="adress">
+      <el-input style="width:500px;"  v-model="ruleForm.adress"></el-input>
+      <el-button type="primary">去定位</el-button>
+      <div>
+        <img src="../../../assets/store_map.jpg" alt="">
+      </div>
+    </el-form-item>
+    <el-form-item label="门店照片" prop="storepic">
+      <div>
+        <div class="storepic">
+          <img src="../../../assets/store_pic.jpg" alt="">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </div>
+      </div>
+    </el-form-item>
+    <el-form-item label="门店Logo" prop="storelogo">
+      <div>
+        <div>
+          <img src="../../../assets/store_logo.jpg" alt="">
+        </div>
+      </div>
     </el-form-item>
   </el-form>
 </template>
 <script>
   export default {
+    components: {
+    },
     data() {
       return {
-        form: {
+        imageUrl: '',
+        ruleForm: {
+          storename: '',
+          phone: '',
           name: '',
+          storetype:'',
+          time:'',
           region: '',
           date1: '',
           date2: '',
@@ -60,17 +79,78 @@
           type: [],
           resource: '',
           desc: ''
+        },
+        rules: {
+          storename: [
+            { required: true, message: '请输入门店名称', trigger: 'blur' },
+            { min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }
+          ],
+          phone: [
+            { required: true, message: '请输入手机号', trigger: 'blur' },
+            { min: 11, max: 11, message: '请输入正确的手机号', trigger: 'blur' }
+          ],
+          name: [
+            { required: true, message: '请输入联系人名称', trigger: 'blur' },
+            { min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
+          ],
+          storetype: [
+            { required: true, message: '请选择活动区域', trigger: 'change' }
+          ],
+          time: [
+            { required: true, message: '请选择营业时间', trigger: 'change' }
+          ],
+          date1: [
+            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+          ],
+          date2: [
+            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+          ],
+          type: [
+            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+          ],
+          resource: [
+            { required: true, message: '请选择活动资源', trigger: 'change' }
+          ],
+          desc: [
+            { required: true, message: '请填写活动形式', trigger: 'blur' }
+          ]
         }
-      }
+      };
     },
     methods: {
-      onSubmit() {
-        console.log('submit!');
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      },
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
       }
     }
   }
 </script>
 
 <style scoped lang="scss">
-  
+  .storepic {
+  }
 </style>
