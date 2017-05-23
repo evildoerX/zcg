ba824465448cd1dd1172eba27312aa2c57c005f0
CH <ch@CHdeMacBook-Pro.local>
2017-05-24 00:36:50 +0800

    增加平台

---
+++ Platform/src/components/layout/AppMain.vue
@@ -0,0 +1,62 @@
+<template>
+	<el-row class="container">
+		<Navbar></Navbar>
+		<Sidebar></Sidebar>
+		<el-col :span="24" class="main">
+			<section class="content-container">
+				<div class="grid-content bg-purple-light">
+					<el-col :span="24" class="breadcrumb-container">
+						<strong class="title"></strong>
+					</el-col>
+					<el-col :span="24" class="content-wrapper">
+						<transition>
+							<router-view></router-view>
+						</transition>
+					</el-col>
+				</div>
+			</section>
+		</el-col>
+	</el-row>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+	export default {
+		components:{
+			Navbar,
+			Sidebar
+		}	
+	}
+
+</script>
+
+<style scoped lang="scss">
+	.container {
+		position: absolute;
+		top: 0px;
+		bottom: 0px;
+		width: 100%;
+		.main {
+			background: #324057;
+			position: absolute;
+			top: 60px;
+			bottom: 0px;
+			overflow: hidden;
+			.content-container {
+				background: #f1f2f7;
+				position: absolute;
+				right: 0px;
+				top: 0px;
+				bottom: 0px;
+				left: 200px;
+				overflow-y: scroll;
+				padding: 20px;
+				.content-wrapper {
+					background-color: #fff;
+					box-sizing: border-box;
+				}
+			}
+		}
+	}
+</style>
---
+++ Platform/src/components/layout/Sidebar.vue
@@ -0,0 +1,64 @@
+<template>
+	<aside class="sidebar">
+		<p class="menu-label">
+      		服务或者项目名称
+    	</p>
+		<el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
+			theme="dark" unique-opened router>
+			<template v-for="(item,index) in $router.options.routes" v-if="!item.hidden" :key="">
+				<el-submenu :index="index+''" v-if="!item.leaf">
+					<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
+					<el-menu-item v-for="child in item.children" :key="item.name" :index="child.path" v-if="!child.hidden">
+						{{child.name}}
+					</el-menu-item>
+				</el-submenu>
+				<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path">
+					<i :class="item.iconCls"></i>
+					{{item.children[0].name}}
+				</el-menu-item>
+			</template>
+		</el-menu>
+	</aside>
+</template>
+
+<script>
+	export default {
+		methods: {
+			handleopen() {
+				//console.log('handleopen');
+			},
+			handleclose() {
+				//console.log('handleclose');
+			},
+			handleselect: function (a, b) {
+			}
+		}
+	}
+
+</script>
+
+<style scoped lang="scss">
+.sidebar {
+	position: fixed;
+	top: 60px;
+	left: 0;
+	bottom: 0;
+	// padding: 20px 0 50px;
+	width: 200px;
+	min-width: 45px;
+	max-height: 100vh;
+	height: calc(100% - 50px);
+	z-index: 1024 - 1;
+	background: #324157;
+	box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
+	overflow-y: auto;
+	overflow-x: hidden;
+	.menu-label {
+    color: #fff;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    padding-left: 5px;
+    font-size: 14px;
+  }
+}
+</style>
